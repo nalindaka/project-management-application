6c8f0de2bbaefa1e2657d311244db0bc2340e249
Nalindaka Hettiarachchi <nalindakasandeep@gmail.com>
2023-11-18 00:51:16 +1000

    Add initial code for new project component

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,28 @@
+export default function NewProject() {
+  return (
+    <div>
+      <menu>
+        <li>Cancel</li>
+        <li>Save</li>
+      </menu>
+      <div>
+        <p>
+          <label htmlFor="">Title</label>
+          <input />
+        </p>
+      </div>
+      <div>
+        <p>
+          <label htmlFor="">Description</label>
+          <textarea></textarea>
+        </p>
+      </div>
+      <div>
+        <p>
+          <label htmlFor="">Due Date</label>
+          <input />
+        </p>
+      </div>
+    </div>
+  );
+}
