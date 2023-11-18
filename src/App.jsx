b282ed2294ed883e35a9a1a50089feeca5dd883d
Nalindaka Hettiarachchi <nalindakasandeep@gmail.com>
2023-11-18 14:11:11 +1000

    Add conditional rendering of NoProjectSelected or NewProject component in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProjectId: null,
+    }));
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  } else if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
